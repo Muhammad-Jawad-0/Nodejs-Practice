9137877c4d80f0a5581806f367d79c040f914ba6
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2025-07-09 01:05:31 +0500

    nodemailer configration and send email using nodemailer

--- Nodejs Biolerplate/src/modules/product/services/post.js
+++ Nodejs Biolerplate/src/modules/product/services/post.js
@@ -1,5 +1,42 @@
 import { addData } from "../db/index.js";
+import nodemailer from "nodemailer";
 
-const postService = async (data) => await addData(data);
+const sendEmail = () => {
+  // Create a transporter object using Gmail SMTP
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "mdjawad11223344@gmail.com",
+      pass: "uvjn gqym curm grdo",
+    },
+  });
+
+  // Email options
+  const mailOptions = {
+    from: "mdjawad11223344@gmail.com",
+    to: "muhammaddjawad.0@gmail.com",
+    subject: "👋 Hello from Node.js 🚀 sending email from nodemailer",
+    text: "This is a test email sent from Node.js using nodemailer. 📧💻",
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("paisa barbad");
+      console.error("❌ Error:", error.message);
+    } else {
+      console.log("email gya bruh");
+      console.log("✅ Email sent:", info.response);
+    }
+  });
+};
+
+const postService = async (data) => {
+    sendEmail()
+  return await addData(data);
+};
 
 export default postService;
